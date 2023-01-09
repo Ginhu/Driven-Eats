0fbc01e1a3512c7758019ea0660481b7d0a5c95d
Sergio Beppu <beppu.sergio@gmail.com>
2023-01-09 10:21:01 -0300

    Botão de finalizar pedido

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+let div;
+let div1;
+let div2;
 function selecionarPrato (prato) {
     const anterior = document.querySelector('.prato-selecionado');
+    /* const anteriorCheck = document.querySelector('.prato-selecionado .ion-icon'); */
+        
 
     if (anterior != null) {
         anterior.classList.remove('prato-selecionado');
+        /* anteriorCheck.classList.add('check-invisivel'); */
+        anterior.querySelector('.ion-icon').classList.add('check-invisivel');
     }
 
-    const div = document.querySelector(prato);
+    div = document.querySelector(prato);
     div.classList.add('prato-selecionado');
+    div.querySelector('.ion-icon').classList.remove('check-invisivel');
+    /* const check = document.querySelector(prato, '.ion-icon');
+    check.classList.remove('check-invisivel'); */
+
+    botao();
 }
 
 function selecionarBebida (bebida) {
@@ -14,10 +26,14 @@ function selecionarBebida (bebida) {
 
     if (anterior != null) {
         anterior.classList.remove('bebida-selecionado');
+        anterior.querySelector('.ion-icon').classList.add('check-invisivel');
     }
 
-    const div = document.querySelector(bebida);
-    div.classList.add('bebida-selecionado');
+    div1 = document.querySelector(bebida);
+    div1.classList.add('bebida-selecionado');
+    div1.querySelector('.ion-icon').classList.remove('check-invisivel');
+
+    botao();
 }
 
 function selecionarSobremesa(sobremesa) {
@@ -25,8 +41,26 @@ function selecionarSobremesa(sobremesa) {
 
     if (anterior != null) {
         anterior.classList.remove('sobremesa-selecionado');
+        anterior.querySelector('.ion-icon').classList.add('check-invisivel');
     }
 
-    const div = document.querySelector(sobremesa);
-    div.classList.add('sobremesa-selecionado');
+    div2 = document.querySelector(sobremesa);
+    div2.classList.add('sobremesa-selecionado');
+    div2.querySelector('.ion-icon').classList.remove('check-invisivel');
+
+    botao();
+}
+
+function botao () {
+    if(div != null && div1 != null && div2 != null) {
+        const botao = document.querySelector('.rodape');
+
+        botao.classList.add('ativado');
+        botao.innerHTML = "Fechar Pedido";
+        botao.disabled = false;
+    }
+}
+
+function finalizarPedido() {
+    
 }
