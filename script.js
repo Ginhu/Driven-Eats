2b2e4cbaf7bb2fd5b9cf7d8d668bb6dba4b613ec
Sergio Beppu <beppu.sergio@gmail.com>
2023-01-09 16:41:29 -0300

    Envio do pedido e Correção automática

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let div;
 let div1;
 let div2;
+let pratoN;
+let pratoValor;
+let bebidaN;
+let valorBebida;
+let sobremesaN;
+let valorSobremesa;
+let valorTotal;
+
 function selecionarPrato (prato) {
     const anterior = document.querySelector('.prato-selecionado');
     /* const anteriorCheck = document.querySelector('.prato-selecionado .ion-icon'); */
@@ -8,15 +16,21 @@ function selecionarPrato (prato) {
 
     if (anterior != null) {
         anterior.classList.remove('prato-selecionado');
-        /* anteriorCheck.classList.add('check-invisivel'); */
         anterior.querySelector('.ion-icon').classList.add('check-invisivel');
     }
 
     div = document.querySelector(prato);
     div.classList.add('prato-selecionado');
     div.querySelector('.ion-icon').classList.remove('check-invisivel');
-    /* const check = document.querySelector(prato, '.ion-icon');
-    check.classList.remove('check-invisivel'); */
+    pratoN = div.querySelector('.nome-prato').innerHTML;
+
+    if (pratoN == "Carne Yin Yang") {
+        pratoValor = 14.9;
+    } else if (pratoN == "Frango Yin Yang") {
+        pratoValor = 15.9;
+    } else if (pratoN == "Peixe Yin Yang") {
+        pratoValor = 16.9;
+    }
 
     botao();
 }
@@ -32,6 +46,15 @@ function selecionarBebida (bebida) {
     div1 = document.querySelector(bebida);
     div1.classList.add('bebida-selecionado');
     div1.querySelector('.ion-icon').classList.remove('check-invisivel');
+    bebidaN = div1.querySelector('.nome-prato').innerHTML;
+
+    if (bebidaN == "Coquinha gelada") {
+        valorBebida = 4.9;
+    } else if (bebidaN == "Guaraná gelada") {
+        valorBebida = 5.9;
+    } else if (bebidaN == "Sprite gelada") {
+        valorBebida = 6.9;
+    }
 
     botao();
 }
@@ -47,6 +70,15 @@ function selecionarSobremesa(sobremesa) {
     div2 = document.querySelector(sobremesa);
     div2.classList.add('sobremesa-selecionado');
     div2.querySelector('.ion-icon').classList.remove('check-invisivel');
+    sobremesaN = div2.querySelector('.nome-prato').innerHTML;
+
+    if (sobremesaN == "Pudim") {
+        valorSobremesa = 7.9;
+    } else if (sobremesaN == "Gelatina") {
+        valorSobremesa = 8.9;
+    } else if (sobremesaN == "Bolo") {
+        valorSobremesa = 9.9;
+    }
 
     botao();
 }
@@ -62,5 +94,46 @@ function botao () {
 }
 
 function finalizarPedido() {
+    document.querySelector('.overlay').classList.add('opacidade');
+    document.querySelector('.confirme-pedido').classList.remove('check-invisivel');
+
+    document.querySelector('.nome-p').innerHTML = pratoN;
+    if (pratoN == "Carne Yin Yang") {
+        document.querySelector('.valor-p').innerHTML = "14,90";
+    } else if (pratoN == "Frango Yin Yang") {
+        document.querySelector('.valor-p').innerHTML = "15,90";
+    } else if (pratoN == "Peixe Yin Yang") {
+        document.querySelector('.valor-p').innerHTML = "16,90";
+    }
+
+    document.querySelector('.nome-b').innerHTML = bebidaN;
+    if (bebidaN == "Coquinha gelada") {
+        document.querySelector('.valor-b').innerHTML = "4,90";
+    } else if (bebidaN == "Guaraná gelada") {
+        document.querySelector('.valor-b').innerHTML = "5,90";
+    } else if (bebidaN == "Sprite gelada") {
+        document.querySelector('.valor-b').innerHTML = "6,90";
+    }
+
+    document.querySelector('.nome-s').innerHTML = sobremesaN;
+    if (sobremesaN == "Pudim") {
+        document.querySelector('.valor-s').innerHTML = "7,90";
+    } else if (sobremesaN == "Gelatina") {
+        document.querySelector('.valor-s').innerHTML = "8,90";
+    } else if (sobremesaN == "Bolo") {
+        document.querySelector('.valor-s').innerHTML = "9,90";
+    }
+
+    valorTotal = ((pratoValor+valorBebida+valorSobremesa).toFixed(2));
+    const valorTotalString = new String(valorTotal);
+    const valorTotalString1 = valorTotalString.replace(".", ",");
+    console.log(valorTotalString);
+    document.querySelector('.valor-t').innerHTML = `R$ ${valorTotalString1}`;
     
 }
+
+function cancelar() {
+    document.querySelector('.overlay').classList.remove('opacidade');
+    document.querySelector('.confirme-pedido').classList.add('check-invisivel');
+}
+
